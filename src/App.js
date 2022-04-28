dca7d5ac20bf9e7bcfe180a9999a48b42a4ae3c0
Kamrul Huda Sattari Saad <skamrul2680@gmail.com>
2022-04-28 23:17:02 +0600

    Basic THings Done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth'
+import auth from './firebase.init'
+
 function App() {
+
+  const [signIn] = useSignInWithGoogle(auth)
+
   return (
     <div>
       <h1>Hello Assignment 11</h1>
+      <button onClick={() => signIn()} >GOogle</button>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  content: ['./src/**/*.{html,js}', './node_modules/tw-elements/dist/js/**/*.js'],
+  plugins: [
+    require('./node_modules/tw-elements/dist/plugin')
+  ]
+}
